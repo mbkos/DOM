4350b70f400bb0b1d008be5e6f019293ad4734f3
rainbow dash <mariana.kos.ifpr.2023@gmail.com>
2025-10-16 10:48:24 -0300

    Add files via upload

---
+++ ex05.js
@@ -0,0 +1,9 @@
+
+/** 5- Crie um novo documento HTML em que o usuário entre com o primeiro valor de
+      uma Progressão Aritmética (PA) a razão da PA e a quantidade de termos da PA e gere a serie.
+     Dica: use uma lista para armazenar a PA e exibir em tela. */
+
+function calcularPA(){
+    var a1, r, n, pa, result
+    
+}
